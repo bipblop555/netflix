44d7aa88844c57d7e8f766d58e360f7e12f1c25d
bipblop555 <tom.bonnet@hotmail.com>
2022-05-05 19:37:46 +0200

    Version Finale test

---
+++ OneDrive/Bureau/V.FINALE_NETSPLIF/src/js/movements.js
@@ -0,0 +1,232 @@
+
+ let film__noir = {
+    films__id: [17136, 1939, 996, 1834, 37992, 17131, 16703, 963, 17136, 22584, 17058,
+                3309, 20367, 17645, 35954, 35954, 20246, 910, 25736, 3767],
+                // 20 FILMS
+    movement__description : "Le film noir est un genre ou un style cinématographique, faisant partie de la catégorie du film criminel. Il prend son inspiration de la littérature policière hard boiled ('roman noir') et d'auteurs comme Dashiell Hammett (considéré comme le fondateur du genre en littérature), Raymond Chandler ou James M. Cain. Tournant autour de thématiques résolument pessimistes et tragiques, comme la fatalité, l'angoisse, la trahison ou la machination, le film noir est soumis à des codes formels et narratifs qui le caractérisent, comme l'usage du suspense, du flashback, de la voix off ; l'emploi de personnages devenus ensuite des stéréotypes comme la femme fatale, le détective, le weak guy (le pauvre type ou l'homme ordinaire), le tueur psychopathe, ou encore le couple en cavale ; dans des lieux généralement urbains et souvent filmés en décors naturels et en noir et blanc."
+}
+
+let film__underground = {
+    films__id: [8327, 46787, 36096, 190384, 474247, 42691, 59048, 356273, 97514, 97513,
+                744896, 649, 1040, 27040, 44445, 155006, 256993, 338497, 264367],
+                // 20 FILMS
+    movement__description : "Le cinéma underground est une variété du cinéma expérimental, essentiellement américaine à l’origine, figurative sans être narrative, qui se situe au croisement entre divers types de cinémas indépendants de contestation (formelle ou politique), liée historiquement à la contreculture. Le terme fait florès surtout dans les années 1960."
+}
+
+let nouvel__hollywood = {
+    films__id: [694, 475, 37247, 10331, 624, 576, 3116, 651, 25188, 1051, 185,
+                238, 838, 829, 510, 103, 703, 11778, 28, 696],
+    movement__description : "Le Nouvel Hollywood (New Hollywood) est un mouvement cinématographique américain de la fin des années 1960 au début des années 1980, qui modernise de façon significative la production de films à Hollywood. Ce cinéma, inscrit dans la contre-culture et influencé par le néoréalisme italien, la modernité européenne et la Nouvelle Vague française, se caractérise par la prise de pouvoir des réalisateurs au sein des grands studios américains et la représentation radicale de thèmes jusqu'alors tabous comme la corruption des pouvoirs politiques, la sexualité, la violence ou le massacre des Indiens."
+}
+
+let cinema__novo = {
+    films__id: [274884, 8353, 188346, 444627, 146075, 146904, 59990, 28525, 836, 212951,
+                67612, 194853, 181092, 67062, 74349, 68173, 243871, 193532, 105347, 253380],
+    movement__description : "Durant les années 1950, sous l'influence du courant néo-réaliste italien, le cinéma brésilien tente de se remettre en question devant la menace hollywoodienne. Il s'agit d'un mélange de néo-réalisme italien et de Nouvelle Vague française. Glauber Rocha est celui qui incarne le mieux ce nouveau mouvement"
+}
+
+let expressionnisme = {
+    films__id: [29267, 5998, 31506, 6793, 653, 10695, 2972, 175716, 184438, 84956, 
+                6131, 2974, 234, 977, 34794, 12206, 631, 27517, 832],
+                // 19 FILMS 
+     movement__description : "Le cinéma expressionniste s'est développé en Allemagne au début du xxe siècle. « Multiple et difficile à saisir », il est « impossible d'en parler sans le situer par rapport à la peinture, au théâtre, à la poésie... » en Allemagne et en Autriche ; il a été « tout autre chose qu'une panoplie de décors bizarres, une métaphysique du Destin ou une réaction politique plus ou moins consciente » "
+}
+
+let dogme__95 = {
+    films__id: [309, 452, 139, 10613, 10621, 42881, 19846, 251504, 251506, 112, 12831,
+                377538, 100607, 504828, 384199, 56314, 99216, 15842, 504847, 191784],
+    movement__description: "Le Dogme95 est lancé en réaction aux superproductions anglo-saxonnes et à l'utilisation abusive d'artifices et d'effets spéciaux aboutissant à des produits formatés, jugés lénifiants et impersonnels. Le but du Dogme95 est de revenir à une sobriété formelle plus expressive, plus originale et jugée plus apte à exprimer les enjeux artistiques contemporains."
+}
+
+let telephones__blancs = {
+    films__id: [437779, 314896, 63182, 827167, 282888, 388882, 444771, 326762, 784734, 827572,
+                824465, 844161, 319133, 784014, 669194, 390528, 345098, 390532, 825272],
+    movement__description: "L'appellation de ce courant est due à la présence quasi-récurrente de téléphones blancs dans au moins une scène des films en question. Le thème de ces films est le plus souvent une romance à l'eau de rose avec des intrigues qui se nouent et se dénouent au... téléphone"
+}
+
+let free__cinema = {
+    films__id: [117470, 50011, 43103, 43112, 28784, 18929, 37230, 25062, 16103, 33765, 76000,
+                26535, 18774, 5769, 64811, 91727, 704, 42744, 24134, 118255],
+    movement__description: "Le Free cinema (littéralement « cinéma libre ») est un courant cinématographique britannique, novateur et contestataire. Jonas Mekas a dit : « Bien que parfois encore trop liés au théâtre et non totalement exempts de clichés, les films du free cinema ont conduit au rajeunissement du cinéma commercial britannique et creusé 'un large fossé entre l'ancien et le nouveau'. »"
+}
+
+let neo__realisme = {
+    films__id: [5156, 307, 8016, 8422, 833, 47796, 405, 4173, 8429, 41464, 12491,
+                31058, 2748, 43469, 5155, ],
+    movement__description: "La principale caractéristique de ce courant est de présenter le quotidien en l'état, en adoptant une position moyenne entre scénario, réalité et documentaire et en se servant souvent de gens de la rue à la place d'acteurs professionnels, en quelque sorte en romançant la « vraie vie »"
+        }
+
+let nouvelle__vague__fr = {
+    films__id :[269, 266, 63880, 84972, 5967, 1628, 406406, 48831, 409979, 80476, 365414,
+                42722, 1714],
+    movement__description : "Le terme apparaît sous la plume de Françoise Giroud dans L'Express du 3 octobre 19571, dans une enquête sociologique sur les phénomènes de génération. La Nouvelle Vague ne se définit pas seulement par ses techniques cinématographiques révolutionnaires pour l'époque, mais par  mais le produit immédiat d'une époque et le fruit de la rencontre de plusieurs jeunes cinéastes."
+    }
+
+let nouveau__cinema__allemand = {
+        films__id: [2000, 62437, 11900, 11710, 6404, 12578, 45560, 216, 10310, 4762,
+            65481, 2204, 12482, 48267, 10308, 107558, 11222, 122524, 42208, 122683],
+        movement__description: "Les réalisateurs du nouveau cinema allemand plaçaient la critique sociale et politique au cœur de leur travail, se démarquant ainsi du film de pur divertissement. Leurs productions, relevant du « cinéma d'auteur », furent en général réalisées en dehors des grands studios de cinéma. Le nouveau cinéma allemand a été influencé par la « Nouvelle Vague » française ainsi que par les mouvements de protestation des années 1968."
+    }
+        
+let nouvelle__vague__japonaise = {
+    films__id: [98193, 17905, 31104, 104441, 61475, 3764, 1556, 78117, 16672, 86664,
+                125222, 98636, 188685, 125217, 76754, 98639, 88527, 111718, 109276, 3764],
+    movement__description: "À l'instar du mouvement français, la Nouvelle Vague japonaise s'étend de la fin des années 1950 au milieu des années 1960. Mais à la différence de sa contrepartie française, la Nouvelle Vague japonaise ne fédère pas des auteurs autour d'une théorie du cinéma ou d'une revue ; ces réalisateurs ont en commun une lecture analytique, parfois critique, des conventions sociales, une certaine prise de distance à l'égard des mythologies cinématographiques établies "
+}
+
+let nouvelle__vague__hongkongaise = {
+    films__id: [212781, 44914, 39865, 104287, 256071, 72445, 313098, 73841, 157236, 156045,
+                170319, 84369, 176217, 210333, 104287, 186940, 404369, 256071, 176213, 157229],
+    movement__description: "Elle naît du système de production infernal de la Shaw Brothers qui ne donne naissance qu'à des œuvres standardisées en grande quantité et bride les réalisateurs à son service qui doivent travailler rapidement et toujours de la même manière. Ceux-ci se réfugient d'abord à la télévision pour s'exprimer avant de revenir au cinéma où ils mettent en place un autre système de production indépendant."
+}
+
+let nouvelle__vague__coreenne = {
+    films__id: [264692, 42506, 70909, 264695, 121462, 264696, 165658, 249481, 265340, 265342,
+                124315, 54111, 10355, 2440, 8653, 54986, 41245, 25650, 18704, 19588],
+    movement__description: "Le cinéma sud-coréen est très dynamique et est un des rares cinémas à concurrencer sur son propre sol le cinéma américain. Diverses écoles cohabitent : des films classiques, qui se fondent sur les traditions et la culture coréenne (Im Kwon-taek), et depuis une dizaine d'années, une nouvelle vague de jeunes réalisateurs, plus ancrés dans le monde moderne. On retient chaque année un nombre important de grands films"
+}
+
+let arr = [film__noir.films__id, film__underground.films__id, nouvel__hollywood.films__id,
+    cinema__novo.films__id, expressionnisme.films__id, dogme__95.films__id, telephones__blancs.films__id,
+    free__cinema.films__id, neo__realisme.films__id, nouvelle__vague__fr.films__id, nouveau__cinema__allemand.films__id,
+    nouvelle__vague__japonaise.films__id, nouvelle__vague__hongkongaise.films__id, nouvelle__vague__coreenne.films__id]
+
+        
+    let $menu__burger = document.querySelector('.menu__burger')
+    let $options = document.querySelectorAll('.courants');
+    
+    let burger = document.querySelector('.menu');
+
+
+
+// const API_URL = "https://api.themoviedb.org/3";
+// const API_KEY = "e0e404ffb5a8f7f879ec7cb0814078dc";
+// const IMAGES_URL = "https://image.tmdb.org/t/p/w500";
+
+// API FETCH 
+function getMoviesById(arr){
+    fetch(`${API_URL}/movie/${arr}?api_key=${API_KEY}`)
+
+    .then((response) => response.json())
+    .then((response) => showMoviesbyId(response))    
+}
+
+// BOUCLE SELECTION
+for (let a = 0; a < $options.length; a++){
+
+    $options[a].addEventListener('click', function(e){
+    console.log("Je click sur ", $options[a].innerText)
+
+    console.log('Mes couilles');
+    console.log($options[a])
+
+    $main.innerHTML = "";
+
+        for(let i = 0; i < arr[a][i]; i++){
+            getMoviesById(arr[a][i]);
+            $wholeMenu.classList.remove('menu-burger-is-active');
+            $blur.classList.add('is-hidden');
+        }
+    }
+)}
+
+// SHOWMOVIES
+function showMoviesbyId(movies) {
+
+$loader.classList.remove("is-active");
+
+
+    // if (movies.total_results === 0) {
+    //   $count.innerText = "pas de résultats";
+    // } else {
+    //   $count.innerText = movies.total_results + " résultats";
+    // }
+  
+    // for (let a = 0; a < $options.length; a++) {
+    //     for(let i = 0; i < arr[a][i]; i++){
+    //         let Allmovies = movies.results;
+  
+      let $mainContainer = document.createElement("div");
+      let $movieTitle = document.createElement("h2");
+      let $movieImage = document.createElement("img");
+      let $movieText = document.createElement("p");
+      //let $movieMoreInfo = document.createElement("button");
+  
+      $mainContainer.classList.add("main-container");
+      $movieImage.classList.add("main-container-image");
+      $movieText.classList.add("main-container-text");
+      //$movieMoreInfo.classList.add("btn-more-information");
+  
+      $main.appendChild($mainContainer);
+      $mainContainer.appendChild($movieTitle);
+      $mainContainer.appendChild($movieImage);
+      $mainContainer.appendChild($movieText);
+      //$mainContainer.appendChild($movieMoreInfo);
+  
+      $movieTitle.textContent = movies.title;
+      $movieImage.setAttribute("src", IMAGES_URL + movies.poster_path);
+      /*if (Allmovies.poster_path === "null") {
+        $movieImage.setAttribute("src", IMAGES_NOT_FOUND);
+      } else {
+        $movieImage.setAttribute("src", IMAGES_URL + Allmovies.poster_path);
+      }*/
+      $movieText.textContent = movies.overview;
+      //$movieMoreInfo.innerText = "More information";
+      $movieImage.addEventListener("click", function () {
+        $player.classList.add("player-is-active");
+        $playerTitle.innerText = movies.title;
+        $playerSynopsis.innerText = movies.overview;
+        $playerImage.setAttribute("src", IMAGES_URL + movies.poster_path);
+        $playerLanguage.innerText =
+          "langue d'origine : " + movies.original_language;
+        $playerReleaseDate.innerText =
+          "date de sortie : " + movies.release_date;
+      });
+        
+    //   let Allmovies = movies.results;
+  
+    //   let $mainContainer = document.createElement("div");
+    //   let $movieTitle = document.createElement("h2");
+    //   let $movieImage = document.createElement("img");
+    //   let $movieText = document.createElement("p");
+    //   //let $movieMoreInfo = document.createElement("button");
+  
+    //   $mainContainer.classList.add("main-container");
+    //   $movieImage.classList.add("main-container-image");
+    //   $movieText.classList.add("main-container-text");
+    //   //$movieMoreInfo.classList.add("btn-more-information");
+  
+    //   $main.appendChild($mainContainer);
+    //   $mainContainer.appendChild($movieTitle);
+    //   $mainContainer.appendChild($movieImage);
+    //   $mainContainer.appendChild($movieText);
+    //   //$mainContainer.appendChild($movieMoreInfo);
+  
+    //   $movieTitle.textContent = movies.title;
+    //   $movieImage.setAttribute("src", IMAGES_URL + movies.poster_path);
+    //   /*if (Allmovies.poster_path === "null") {
+    //     $movieImage.setAttribute("src", IMAGES_NOT_FOUND);
+    //   } else {
+    //     $movieImage.setAttribute("src", IMAGES_URL + Allmovies.poster_path);
+    //   }*/
+    //   $movieText.textContent = movies.overview;
+    //   //$movieMoreInfo.innerText = "More information";
+    //   $movieImage.addEventListener("click", function () {
+    //     $player.classList.add("player-is-active");
+    //     $playerTitle.innerText = movies.title;
+    //     $playerSynopsis.innerText = movies.overview;
+    //     $playerImage.setAttribute("src", IMAGES_URL + movies.poster_path);
+    //     $playerLanguage.innerText =
+    //       "langue d'origine : " + movies.original_language;
+    //     $playerReleaseDate.innerText =
+    //       "date de sortie : " + movies.release_date;
+    //   });
+    
+
+    $lastVisiblePage = movies.total_pages;
+    $counterPage();
+    $returnTopPage();
+  }
+
+
+
